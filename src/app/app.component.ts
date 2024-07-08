6662e90c5b7c5dad7696b46f6ae9054ab37809e0
amitkrsaini007 <amitkrsaini@gmail.com>
2024-07-08 15:28:40 +0500

    hii

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,14 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterLink, RouterOutlet } from '@angular/router';
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { LoginComponent } from './components/login/login.component';
+import { SignupComponent } from './components/signup/signup.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,HeaderComponent,FooterComponent,LoginComponent,SignupComponent,RouterLink],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
